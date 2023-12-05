2c3b3088c57cc5b2a364c62b73842db1a73769b0
Dmitry Pechiuz <initdie@gmail.com>
2023-12-05 14:01:48 +0200

    done pz 5

--- app/js/bundle.js
+++ app/js/bundle.js
@@ -1,60 +1,61 @@
-const actions = ['+', '-', '*', '/', '.', '%'];
+const actions = ["+", "-", "*", "/", ".", "%"];
 
 const dashboard = document.getElementById("dashboard");
 
 function printAction(val) {
-  if (val === '+/-') {
-    let firstDigit = dashboard.value[0]
-    if (firstDigit === '-') {
-      dashboard.value = dashboard.value.slice(1, dashboard.value.length)
+  if (val === "+/-") {
+    let firstDigit = dashboard.value[0];
+    if (firstDigit === "-") {
+      dashboard.value = dashboard.value.slice(1, dashboard.value.length);
     } else {
-      dashboard.value = '-' + dashboard.value
+      dashboard.value = "-" + dashboard.value;
     }
-  } else if (actions.includes(dashboard.value[dashboard.value.length - 1])
-    || dashboard.value.length === 0) {
+  } else if (
+    actions.includes(dashboard.value[dashboard.value.length - 1]) ||
+    dashboard.value.length === 0
+  ) {
   } else {
-    dashboard.value += val
+    dashboard.value += val;
   }
 }
 
 function printDigit(val) {
-  dashboard.value += val
+  dashboard.value += val;
 }
 
 function solve() {
-  let expression = dashboard.value
-  dashboard.value = math.evaluate(expression)
+  let expression = dashboard.value;
+  dashboard.value = math.evaluate(expression);
 }
 
 function clr() {
-  dashboard.value = ''
+  dashboard.value = "";
 }
 
 function setTheme(themeName) {
-  localStorage.setItem('theme', themeName);
-  document.querySelector('body').className = themeName;
+  localStorage.setItem("theme", themeName);
+  document.querySelector("body").className = themeName;
 }
 
 function toggleTheme() {
-  let theme = localStorage.getItem('theme');
+  let theme = localStorage.getItem("theme");
 
-  if (theme === 'theme-second') {
-    theme = 'theme-one'
-  } else if (theme === 'theme-one') {
-    theme = 'theme-second'
+  if (theme === "theme-second") {
+    theme = "theme-one";
+  } else if (theme === "theme-one") {
+    theme = "theme-second";
   }
   setTimeout(() => {
     setTheme(theme);
-  }, 500)
+  }, 500);
 }
 
 function save() {
-  localStorage.setItem('result', dashboard.value);
+  localStorage.setItem("result", dashboard.value);
 }
 
 function paste() {
-  printDigit(localStorage.getItem('result'))
+  printDigit(localStorage.getItem("result"));
 }
 
-
-setTheme('theme-one');
+setTheme("theme-one");
