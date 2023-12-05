5ad78761deb2554dd2d849265f2869757a061271
Akimi-coder <vladaxl9@gmail.com>
2023-12-05 12:36:36 +0200

    PZ5 done Linnyk

--- cypress/e2e/calculator.spec.cy.js
+++ cypress/e2e/calculator.spec.cy.js
@@ -1,24 +1,52 @@
 describe('e2e test for calculator app', () => {
-  let bnt1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0;
+  let btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0;
   let btnPlus, btnMinus, btnEqual, dashboard;
+  let btnMultiply, btnDivide, btnDot, btnPercent, btnClear, btnSave, btnPaste, btnToggleTheme,btnPlusMinus;
+
 
   before(() => {
     cy.visit('http://localhost:3000/');
-    cy.get('[data-test="btn-1"]').then(($el) => {
-      bnt1 = $el
-    });
+
     btn2 = cy.get('[data-test="btn-2"]').then(($el) => {
       btn2 = $el
     });
+    cy.get('[data-test="btn-1"]').then(($el) => {
+      btn1 = $el });
+    cy.get('[data-test="btn-3"]').then(($el) => {
+       btn3 = $el });
+    cy.get('[data-test="btn-4"]').then(($el) => {
+       btn4 = $el });
+    cy.get('[data-test="btn-5"]').then(($el) => {
+       btn5 = $el });
     cy.get('[data-test="btn-6"]').then(($el) => {
-      btn6 = $el
-    });
+       btn6 = $el });
+    cy.get('[data-test="btn-7"]').then(($el) => {
+       btn7 = $el });
+    cy.get('[data-test="btn-8"]').then(($el) => {
+       btn8 = $el });
     cy.get('[data-test="btn-9"]').then(($el) => {
-      btn9 = $el
-    });
+       btn9 = $el });
+    cy.get('[data-test="btn-0"]').then(($el) => {
+       btn0 = $el });
     cy.get('[data-test="btn-plus"]').then(($el) => {
       btnPlus = $el
     });
+    cy.get('[data-test="btn-multiply"]').then(($el) => {
+       btnMultiply = $el });
+    cy.get('[data-test="btn-slash"]').then(($el) => {
+       btnDivide = $el });
+    cy.get('[data-test="btn-dot"]').then(($el) => 
+    { btnDot = $el });
+    cy.get('[data-test="btn-percentages"]').then(($el) => {
+       btnPercent = $el });
+    cy.get('[data-test="btn-clr"]').then(($el) => {
+       btnClear = $el });
+    cy.get('[data-test="btn-save"]').then(($el) => {
+       btnSave = $el });
+    cy.get('[data-test="btn-paste"]').then(($el) => {
+       btnPaste = $el });
+    cy.get('[data-test="btn-theme"]').then(($el) => {
+       btnToggleTheme = $el });
     cy.get('[data-test="btn-minus"]').then(($el) => {
       btnMinus = $el
     });
@@ -28,6 +56,10 @@ describe('e2e test for calculator app', () => {
     cy.get('[data-test="btn-equal"]').then(($el) => {
       btnEqual = $el
     });
+    cy.get('[data-test="btn-plus-minus"]').then(($el) => {
+      btnPlusMinus = $el
+    });
+    
   });
 
   it('should check expression 2+9-6', () => {
@@ -50,39 +82,130 @@ describe('e2e test for calculator app', () => {
     })
 
   })
+
   it('should check result of expression 1+2+3+4-5-6-7-8-9', () => {
-    throw new Error("Write your code here")
-  })
+    btnClear.click();
+    btn1.click();
+    btnPlus.click();
+    btn2.click();
+    btnPlus.click();
+    btn3.click();
+    btnPlus.click();
+    btn4.click();
+    btnMinus.click();
+    btn5.click();
+    btnMinus.click();
+    btn6.click();
+    btnMinus.click();
+    btn7.click();
+    btnMinus.click();
+    btn8.click();
+    btnMinus.click();
+    btn9.click();
+    btnEqual.click();
+  
+    cy.wrap(dashboard).invoke('val').then(val => {
+      expect(val).to.equal('-25');
+    });
+  });
+
   it('should check result expression 10/10', () => {
-    throw new Error("Write your code here")
+    btnClear.click();
+    btn1.click();
+    btn0.click();
+    btnDivide.click();
+    btn1.click();
+    btn0.click();
+    btnEqual.click();
+
+    cy.wrap(dashboard).invoke('val').then(val => {
+      expect(val).to.equal('1');
+    });
   })
 
   it('should check result expression 0.5*10', () => {
-    throw new Error("Write your code here")
+    btnClear.click();
+    btn0.click();
+    btnDot.click();
+    btn5.click();
+    btnMultiply.click();
+    btn1.click();
+    btn0.click();
+    btnEqual.click();
+  
+    cy.wrap(dashboard).invoke('val').then(val => {
+      expect(val).to.equal('5');
+    });
   })
 
   it('should check result expression -0.1*99', () => {
-    throw new Error("Write your code here")
-  })
+    btnClear.click();
 
+    btnPlusMinus.click();
+    btn0.click();
+    btnDot.click();
+    btn1.click();
+    btnMultiply.click();
+    btn9.click();
+    btn9.click();
+    btnEqual.click();
+  
+    cy.wrap(dashboard).invoke('val').then(val => {
+      expect(val).to.equal('-9.9');
+    });
+  });
+  
   it('should calculate 10 percent of 1,000', () => {
-    throw new Error("Write your code here")
-  })
+    btnClear.click();
+    btn1.click();
+    btn0.click();
+    btnPercent.click();
+    btn1.click();
+    btn1.click();
+    btn1.click();
+    btnEqual.click();
 
+    cy.wrap(dashboard).invoke('val').then(val => {
+      expect(val).to.equal('10');
+    });
+  });
+  
   it('should check clear button', () => {
-    throw new Error("Write your code here")
-  })
-
+    btnClear.click();
+    btn1.click();
+    btn2.click();
+    btn3.click();
+    btnClear.click();
+  
+    cy.wrap(dashboard).invoke('val').then(val => {
+      expect(val).to.equal('');
+    });
+  });
+  
   it('should check save button', () => {
-    throw new Error("Write your code here")
-  })
-
+    btnClear.click();
+    btn1.click();
+    btnSave.click();
+  
+    cy.reload(); 
+    btnPaste.click();
+  
+    cy.wrap(dashboard).invoke('val').then(val => {
+      expect(val).to.equal('11');
+    });
+  });
+  
   it('should check paste button', () => {
-    throw new Error("Write your code here")
-  })
-
-  it('should check change theme, toggle theme button', () => {
-    throw new Error("Write your code here")
-  })
+    btnPaste.click();
+  
+    cy.wrap(dashboard).invoke('val').then(val => {
+      expect(val).to.equal('11');
+    });
+  });
+  
+  it('should check change theme, toggle theme button', () => {  
+    btnToggleTheme.click()
+    cy.get('body').should('have.class', 'theme-one');
+  });
 
 })
